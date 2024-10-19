2bb08bdf72b9b442b955d3670f383e56bfd559ff
FernandoJ07 <fernandogerdez@gmail.com>
2024-10-19 14:24:03 -0500

    Add Docker-compose file, Express and Mongoose dependencies

--- backend-citas/app.js
+++ backend-citas/app.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
+
+const mongoURI = 'mongodb://127.0.0.1:27017/citas';
+
+mongoose.connect(mongoURI)
+  .then(() => console.log('Conectado a MongoDB'))
+  .catch((err) => console.error('Error de conexión a MongoDB:', err));
+
 
 app.get('/', (req, res) => {
   res.send('¡Hola, mundo!');
