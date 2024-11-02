578095d1e398e930db7141dac757bb030f2fe65c
Jean Paul Delgado <jean.delgado@uao.edu.co>
2024-11-02 11:17:07 -0500

    Simplified props passing to child component

--- fronted-citas/src/components/ListarCitas.jsx
+++ fronted-citas/src/components/ListarCitas.jsx
@@ -1,13 +1,13 @@
 import Horario from './Horario'
 
-function ListarCitas({disponibilidad, modal, inicio, fin}) {
+function ListarCitas({disponibilidad}) {
 
     return(
         
         <>
         {disponibilidad.map((item, idx) => {
 
-            return(<div key={idx} className='w-fit h-fit mt-4 p-4 rounded-lg bg-slate-200'><Horario modal={modal} franja={item} inicio={inicio} fin={fin}/></div>)
+            return(<div key={idx} className='w-fit h-fit mt-4 p-4 rounded-lg bg-slate-200'><Horario franja={item}/></div>)
         
         })}
         </>
